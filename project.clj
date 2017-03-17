30ce13b6581ce69b9020aaba2c9cb05e17957d22
Joshua Davey <joshua.davey@sonian.net>
2017-03-17 01:58:23 +0000

    Modernize test matrix

--- project.clj
+++ project.clj
@@ -3,10 +3,9 @@
   :url "https://github.com/joegallo/doric"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.6.0"]
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                                   [org.apache.poi/poi "3.10.1"]]}}
-  :aliases {"all" ["with-profile" "dev,1.2:dev,1.3:dev,1.4:dev,1.5:dev"]})
+  :aliases {"all" ["with-profile" "dev,1.6:dev,1.7:dev,1.8:dev"]})
